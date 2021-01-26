9805ddddf11d88fba5cef5b66c85d7406fecd52f
Michael Hungbo <hungbomichael@gmail.com>
2021-01-26 01:10:04 +0100

    make edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import {Link} from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello Gatsby</div>
+  const mystring = "Hello Gatsby";
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText={mystring}/>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  );
 }
